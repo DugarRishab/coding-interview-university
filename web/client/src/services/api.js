b6cfc6df7305888ae88466ce46acb5da49d2efca
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-01-15 20:31:46 +0530

    fixed a bug in google auth

--- web/client/src/services/api.js
+++ web/client/src/services/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-	// baseURL: "http://localhost:8000/api/v1",
+	baseURL: "http://localhost:8000/api/v1",
 	withCredentials: true,
-	baseURL: "https://cui.onrender.com/api/v1/"
+	// baseURL: "https://cui.onrender.com/api/v1/"
 });
 
-export const googleAuth = (code) => api.get(`/auth/google?code=${code}`);
+export const googleAuth = (code) => api.get(`./auth/google?code=${code}`);
 export const loginAuth = (payload) => api.post('./auth/login', payload);
 export const signupAuth = (payload) => api.post("./auth/signup", payload);
 export const logoutAuth = () => api.get("./auth/logout");
